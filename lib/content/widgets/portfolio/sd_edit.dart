bb6526a1ceade94646ad00686a0b5d5aa80425cc
MrNtlu <97burakfidan97@gmail.com>
2022-02-05 18:49:07 +0300

    Subscription Edit color picker added.

--- lib/content/widgets/portfolio/sd_edit.dart
+++ lib/content/widgets/portfolio/sd_edit.dart
@@ -1,13 +1,17 @@
 import 'package:asset_flutter/content/pages/portfolio/portfolio_page.dart';
 import 'package:asset_flutter/content/widgets/portfolio/sd_edit_bill_cycle.dart';
+import 'package:asset_flutter/content/widgets/portfolio/sd_edit_color_picker.dart';
 import 'package:asset_flutter/content/widgets/portfolio/sd_edit_date_picker.dart';
 import 'package:asset_flutter/content/widgets/portfolio/sd_edit_header.dart';
 import 'package:flutter/material.dart';
 
 class SubscriptionDetailsEdit extends StatefulWidget {
   final TestSubscriptionData _data;
+  late final SDEditColorPicker colorPicker;
 
-  const SubscriptionDetailsEdit(this._data, {Key? key}) : super(key: key);
+  SubscriptionDetailsEdit(this._data, {Key? key}) : super(key: key){
+    colorPicker = SDEditColorPicker(_data);
+  }
 
   @override
   State<SubscriptionDetailsEdit> createState() => _SubscriptionDetailsEditState();
@@ -48,8 +52,11 @@ class _SubscriptionDetailsEditState extends State<SubscriptionDetailsEdit> {
             ),
           ),
         ),
+        Container(
+          margin: const EdgeInsets.fromLTRB(12, 8, 8, 4),
+          child: widget.colorPicker
+        ),
         const Divider(thickness: 1),
-        //Color picker https://www.youtube.com/watch?v=Q67qNgSVVpA&t=1414s
       ],
     );
   }
---
+++ lib/content/widgets/portfolio/sd_edit_color_picker.dart
@@ -0,0 +1,47 @@
+import 'package:asset_flutter/content/pages/portfolio/portfolio_page.dart';
+import 'package:flutter/material.dart';
+
+class SDEditColorPicker extends StatefulWidget {
+  final TestSubscriptionData _data;
+  late Color selectedColor;
+
+  SDEditColorPicker(this._data, {Key? key}) : super(key: key) {
+    selectedColor = Color(_data.color);
+  }
+
+  @override
+  State<SDEditColorPicker> createState() => _SDEditColorPickerState();
+}
+
+class _SDEditColorPickerState extends State<SDEditColorPicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        for (var color in TestData.testChartStatsColor)
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                widget.selectedColor = color;
+                widget._data.color = color.value;
+              });
+            },
+            child: Container(
+              padding: const EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                border: Border.all(
+                    color: (widget.selectedColor == color)
+                        ? Colors.orangeAccent
+                        : Colors.transparent),
+                shape: BoxShape.circle,
+              ),
+              child: CircleAvatar(
+                radius: 10,
+                backgroundColor: color,
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+}
